01eeb0010938a2f44cf5a02c53efc1026a37412f
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-05-30 21:47:47 +0600

    add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+import Drawer from "./components/Drawer/Drawer";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
+import Toolbar from "./components/Toolbar/Toolbar";
 
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="App">
-      <Nav/>
+      <Toolbar toggleDrawer={toggleDrawer}/>
+      <Drawer open={drawerOpen} toggle={toggleDrawer} />
       <Header/>
       <Footer/>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
+import headerImage from "../../assets/san-1.png";
 function Header() {
-    return ( 
-        <header className={classes.Header}>
-            <h1>САНТЕХНИК 24/7 В АЛМАТЫ</h1>
-            <p>Все виды сантехнических услуг — недорого</p>
-        </header>
-     );
+  return (
+    <header className={classes.Header}>
+      <div className={classes.Header__item}>
+        <img src={headerImage} alt="HEDER" className={classes.Header__img} />
+        <div className={classes.Header__info}>
+          <h1>САНТЕХНИК 24/7 В БИШКЕК</h1>
+          <p>Все виды сантехнических услуг — недорого</p>
+            <button className={classes.Header__button}>
+                +996123456789
+            </button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
