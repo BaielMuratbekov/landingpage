517034a2f1e4527012c5156032a50044814ae945
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-05-30 18:02:25 +0600

    Add three simple components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import classes from './Header.module.css'
+function Header() {
+    return ( 
+        <header className={classes.Header}>
+            Hello world!
+        </header>
+     );
+}
+
+export default Header;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,10 @@
+import classes from './Nav.module.css'
+function Nav() {
+    return ( 
+        <nav className={classes.Nav}>
+            nav
+        </nav>
+     );
+}
+
+export default Nav;
