6cc6a658bb1fe1a27e3aeaf3d9725baeeca7d486
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-01 14:20:24 +0600

    add contacts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header/Header";
-import Toolbar from './components/Toolbar/Toolbar'
-import Drawer from './components/Drawer/Drawer'
-import Footer from './components/Footer/Footer'
+import Toolbar from "./components/Toolbar/Toolbar";
+import Drawer from "./components/Drawer/Drawer";
+import Footer from "./components/Footer/Footer";
 import Servis from "./components/Servis/Servis";
 import { useState } from "react";
 import About from "./components/About/About";
 import Contacts from "./components/Contacts/Contacts";
+import Back from "./components/Back/Back";
 function App() {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
@@ -14,14 +15,14 @@ function App() {
   }
   return (
     <div className="App">
-      
-        <Toolbar toggleDrawer={toggleDrawer} />
-        <Drawer open={drawerOpen} toggle={toggleDrawer} />
-        <Header />
-        <Servis />
-        <About/>
-        <Contacts/>
-        <Footer/>
+      <Toolbar toggleDrawer={toggleDrawer} />
+      <Drawer open={drawerOpen} toggle={toggleDrawer} />
+      <Header />
+      <Servis />
+      <About />
+      <Contacts />
+      <Back />
+      <Footer />
     </div>
   );
 }
