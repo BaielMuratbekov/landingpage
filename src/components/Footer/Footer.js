dcf812a9e6d4d4c890772f7bad877f49b45a4069
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-06-01 14:55:02 +0600

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,11 @@ import classes from './Footer.module.css'
 function Footer() {
     return (  
         <footer className={classes.Footer}>
-        
+            <ul>
+                <li><a href='/'><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href='/'><i class="fa-brands fa-whatsapp"></i></a></li>
+                <li><a href='/'><i class="fa-brands fa-telegram"></i></a></li>
+            </ul>
         </footer>
     );
 }
